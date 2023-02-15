78bc32eecb4e04d6825688669a425b3e26df9b5f
notsoloud <kanmohan2911@gmail.com>
2023-02-15 23:46:48 +0530

    Driver

---
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -0,0 +1,38 @@
+package com.driver.services.impl;
+
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.ServiceProvider;
+import com.driver.repository.AdminRepository;
+import com.driver.repository.CountryRepository;
+import com.driver.repository.ServiceProviderRepository;
+import com.driver.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    AdminRepository adminRepository1;
+
+    @Autowired
+    ServiceProviderRepository serviceProviderRepository1;
+
+    @Autowired
+    CountryRepository countryRepository1;
+
+    @Override
+    public Admin register(String username, String password) {
+    }
+
+    @Override
+    public Admin addServiceProvider(int adminId, String providerName) {
+    }
+
+    @Override
+    public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+    }
+}
---
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package com.driver.services.impl;
+
+import com.driver.model.Country;
+import com.driver.model.ServiceProvider;
+import com.driver.model.User;
+import com.driver.repository.CountryRepository;
+import com.driver.repository.ServiceProviderRepository;
+import com.driver.repository.UserRepository;
+import com.driver.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository3;
+    @Autowired
+    ServiceProviderRepository serviceProviderRepository3;
+    @Autowired
+    CountryRepository countryRepository3;
+
+    @Override
+    public User register(String username, String password, String countryName) throws Exception{
+
+    }
+
+    @Override
+    public User subscribe(Integer userId, Integer serviceProviderId) {
+
+    }
+}
